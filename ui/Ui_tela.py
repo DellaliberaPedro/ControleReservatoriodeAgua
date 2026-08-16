7209dc99b6f5443ae217ed2ad02bd4158fa58e49
Pedro Henrique Devens Dellalibera <pedro.dellalibera5@yahoo.com>
2026-08-16 19:46:59 -0300

    Primeiros Ajustes

--- ui/Ui_tela.py
+++ ui/Ui_tela.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLCDNumber,
+    QLabel, QMainWindow, QMenuBar, QSizePolicy,
+    QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -25,6 +26,48 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.layout_principal = QVBoxLayout(self.centralwidget)
+        self.layout_principal.setObjectName(u"layout_principal")
+        self.group_telemetria = QGroupBox(self.centralwidget)
+        self.group_telemetria.setObjectName(u"group_telemetria")
+        self.grid_telemetria = QGridLayout(self.group_telemetria)
+        self.grid_telemetria.setObjectName(u"grid_telemetria")
+        self.lbl_temperatura = QLabel(self.group_telemetria)
+        self.lbl_temperatura.setObjectName(u"lbl_temperatura")
+
+        self.grid_telemetria.addWidget(self.lbl_temperatura, 0, 0, 1, 1)
+
+        self.lcd_temperatura = QLCDNumber(self.group_telemetria)
+        self.lcd_temperatura.setObjectName(u"lcd_temperatura")
+        self.lcd_temperatura.setDigitCount(5)
+
+        self.grid_telemetria.addWidget(self.lcd_temperatura, 0, 1, 1, 1)
+
+        self.lbl_tds = QLabel(self.group_telemetria)
+        self.lbl_tds.setObjectName(u"lbl_tds")
+
+        self.grid_telemetria.addWidget(self.lbl_tds, 1, 0, 1, 1)
+
+        self.lcd_tds = QLCDNumber(self.group_telemetria)
+        self.lcd_tds.setObjectName(u"lcd_tds")
+        self.lcd_tds.setDigitCount(5)
+
+        self.grid_telemetria.addWidget(self.lcd_tds, 1, 1, 1, 1)
+
+        self.lbl_turbidez = QLabel(self.group_telemetria)
+        self.lbl_turbidez.setObjectName(u"lbl_turbidez")
+
+        self.grid_telemetria.addWidget(self.lbl_turbidez, 2, 0, 1, 1)
+
+        self.lcd_turbidez = QLCDNumber(self.group_telemetria)
+        self.lcd_turbidez.setObjectName(u"lcd_turbidez")
+        self.lcd_turbidez.setDigitCount(5)
+
+        self.grid_telemetria.addWidget(self.lcd_turbidez, 2, 1, 1, 1)
+
+
+        self.layout_principal.addWidget(self.group_telemetria)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -41,5 +84,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Controle de Reservat\u00f3rio de \u00c1gua", None))
+        self.group_telemetria.setTitle(QCoreApplication.translate("MainWindow", u"Telemetria", None))
+        self.lbl_temperatura.setText(QCoreApplication.translate("MainWindow", u"Temperatura (\u00b0C)", None))
+        self.lbl_tds.setText(QCoreApplication.translate("MainWindow", u"TDS (ppm)", None))
+        self.lbl_turbidez.setText(QCoreApplication.translate("MainWindow", u"Turbidez (NTU)", None))
     # retranslateUi
 
